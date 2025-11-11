ae840b1ac324943bcdb6f524d06ad8444260fa14
Barshat Pradhan <barshatpradhan321@gmail.com>
2025-11-11 11:05:50 -0500

    little config with both files for production

--- Frontend/src/api.ts
+++ Frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { GitHubRepo, Favorite } from './types';
 
-const API_BASE = '/api';
+const API_BASE = import.meta.env.VITE_API_URL || '/api';
 
 const api = axios.create({ baseURL: API_BASE });
 
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,36 +1,3 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import authRoutes from './Routes/auth';
-// import favoritesRoutes from './Routes/favorites';
-// import serverless from "serverless-http";
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(cors());
-// app.use(express.json());
-
-// //Routes 
-// app.use('/api/auth', authRoutes);
-// app.use('/api/favorites', favoritesRoutes);
-
-// //MongoDB connection
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/github-explorer' )
-// .then(() => console.log('MongoDB connected'))
-// .catch((err) => console.error('MongoDB connection error:', err));
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on port http://localhost:${PORT}`)
-// });
-
-// // Export as a Vercel serverless function
-// export const handler = serverless(app);
-
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -45,7 +12,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173', // local dev
+    'https://search-git-hub-repo.onrender.com'
+  ],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // Routes
@@ -73,7 +47,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`✅ Server running on port ${PORT}`);
+    console.log(`✅ Server running on port http://localhost:${PORT}`);
 });
 
 // Export as a Vercel serverless function
